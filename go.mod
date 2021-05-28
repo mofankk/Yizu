25b3f3894a67d8b7aa70f2992de9d9e6f538a956
Youpeng <mofankk@gmail.com>
2021-05-28 09:47:53 +0800

    add casbin

--- go.mod
+++ go.mod
@@ -3,10 +3,12 @@ module yizu
 go 1.16
 
 require (
+	github.com/casbin/gorm-adapter/v3 v3.2.12 // indirect
 	github.com/gin-gonic/gin v1.7.1
 	github.com/go-redis/redis/v8 v8.8.3
 	github.com/google/uuid v1.0.0
 	github.com/jung-kurt/gofpdf v1.16.2
+	github.com/lib/pq v1.8.0 // indirect
 	github.com/sirupsen/logrus v1.8.1
 	github.com/tencentcloud/tencentcloud-sdk-go v1.0.166
 	gorm.io/driver/postgres v1.1.0
---
+++ util/casbin.go
@@ -0,0 +1,22 @@
+package yizuutil
+
+import (
+	"github.com/casbin/casbin/v2"
+	gormadapter "github.com/casbin/gorm-adapter/v3"
+	log "github.com/sirupsen/logrus"
+)
+
+var e *casbin.Enforcer
+
+func InitCasbin() {
+	db, err := GetDB()
+	if err != nil {
+		log.Errorf("数据库连接失败: %v", err)
+	}
+	a, _ := gormadapter.NewAdapterByDB(db)
+	e, _ = casbin.NewEnforcer("examples/rbac_model.conf", a)
+}
+
+func GetCasbin() *casbin.Enforcer {
+	return e
+}
